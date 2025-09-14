f96908a66f2e5fde6ff25cfd53b0eb45ec1f8a80
Johannes Aas <johannes.hansen.aas@gmail.com>
2025-09-15 01:36:27 +0200

    Switch to Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,15 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
+	extensions: ['.svelte', '.svx'], // keep mdsvex extensions
+
 	preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+
+	kit: {
+		adapter: adapter(), // use Vercel adapter instead of auto
+	}
 };
 
 export default config;
