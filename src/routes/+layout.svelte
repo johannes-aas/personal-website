2acac2cac4d65ef2e2618b54fac8cad5fe923788
Johannes Aas <johannes.hansen.aas@gmail.com>
2025-09-27 23:41:19 +0200

    Add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 </script>
