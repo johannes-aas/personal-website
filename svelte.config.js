8b9c57be068cfdc5ec8dd2a88eada452f5696aa5
Johannes Aas <johannes.hansen.aas@gmail.com>
2025-09-29 18:16:36 +0200

    Add markdown support

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,10 @@ import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.svx'], // keep mdsvex extensions
-
+	extensions: ['.svelte', '.svx'],
 	preprocess: [mdsvex()],
-
 	kit: {
-		adapter: adapter() // use Vercel adapter instead of auto
+		adapter: adapter()
 	}
 };
 
